37a4dda25a94596b59d532f94ad9ee902ee69d2c
Shubham Thakur <91404097+ShubhamThakur13@users.noreply.github.com>
2026-04-10 16:16:35 +0530

    Update

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Blog from './Blog'
+import { MyBlogData } from '../context/BlogContext'
+import { User } from 'lucide-react';
 
 const BlogList = () => {
+ let {Users} =  useContext(MyBlogData);
   return (
     <div>
-     Blog List... 
+      <div className="w-screen top flex items-center justify-evenly">
+        <h2 className='text-2xl font-bold'>Latest Articles</h2>
+        <p>3 articles</p>
+      </div>
+      <div>
+        {Users.map((elem)=>{
+        })}
+      </div>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import BlogList from '../components/BlogList'
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className=' h-screen flex flex-col items-center gap-10'>
+      <div className="top w-[35%] mt-[8%] flex flex-col gap-5">
+        <h1 className='text-5xl font-bold text-center'>Welcome to <span className='text-[#0056A4]'>Inkwell</span></h1>
+        <p className=' text-xl text-center'>Discover thoughtful articles on technologu, programming, and software engineering from passionate writers.</p>
+      </div>
+      <BlogList />
     </div>
   )
 }
